3bb975ca6a7853347039a62fe14e68d4887f5306
Wang Guangzu <wgz199154@gmail.com>
2023-06-28 02:01:11 +0800

    debug for railway deployment

--- backend/src/main/java/ibf/miniproject/ecommerce/config/AppConfig.java
+++ backend/src/main/java/ibf/miniproject/ecommerce/config/AppConfig.java
@@ -3,6 +3,7 @@ package ibf.miniproject.ecommerce.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -10,6 +11,8 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 @Configuration
 public class AppConfig {
@@ -42,6 +45,14 @@ public class AppConfig {
                 .withEndpointConfiguration(ep)
                 .withCredentials(new AWSStaticCredentialsProvider(cred))
                 .build();
+    }
+
+    @Value("${mongo.url}")
+    private String mongoUrl;
+    @Bean
+    public MongoTemplate createMongoTemplate() {
+        MongoClient client = MongoClients.create(mongoUrl);
+        return new MongoTemplate(client, "yourdbname");
     }
     
 }
